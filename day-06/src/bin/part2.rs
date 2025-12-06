40ec273c45bb78c56c94e43454aec491694d2ea6
Sekuloski <bojan@sekuloski.mk>
2025-12-06 13:20:16 +0100

    feat: Day 6 part 1 solved

---
+++ day-06/src/bin/part2.rs
@@ -0,0 +1,31 @@
+fn main() {
+    let input = include_str!("./input.txt");
+    let output = part2(&remove_trailing_newline(input));
+    dbg!(output);
+}
+
+fn remove_trailing_newline(input: &str) -> String {
+    input.strip_suffix("\r\n")
+        .or(input.strip_suffix("\n"))
+        .unwrap_or(input)
+        .to_string()
+}
+
+fn part2(input: &str) -> String {
+    input.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part2() {
+        let result = part2(
+            ""
+        );
+        dbg!(&result);
+        assert_eq!(result, result);
+    }
+}
+
